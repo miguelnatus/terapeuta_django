233ed0a07ca1f7199a3d94c8619b98b93ac47e43
Miguel Natus <miguelnatus@gmail.com>
2026-01-14 15:49:30 -0300

    terapeuta

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path ,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('landing.urls')),
+    path('d-rel-1/', include('landing.urls')),
 ]
