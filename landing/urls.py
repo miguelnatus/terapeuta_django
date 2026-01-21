74c42dcf9f1883b74f3b6c96b083e2482dfd2d68
Miguel Natus <miguelnatus@gmail.com>
2026-01-20 23:49:06 -0300

    de-fem-1

--- landing/urls.py
+++ landing/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import  Drel1, Dtrel1
+from .views import  Drel1, Dtrel1, Drel2, Dfem1
 
 urlpatterns = [
     path('d-rel-1/', Drel1.as_view(), name='drel1'),
     path('dt-rel-1/', Dtrel1.as_view(), name='dtrel1'), 
+    path('d-rel-2/', Drel2.as_view(), name='drel2'),
+    path('d-fem-1/', Dfem1.as_view(), name='dfem1'),
+    #path('d-fem-2/', Dfem1.as_view(), name='dfem2'),
+    #path('d-fem-3/', Dfem1.as_view(), name='dfem3'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -3,6 +3,12 @@ from django.views.generic import TemplateView
 
 class Drel1(TemplateView):
     template_name = 'home/d-rel-1.html'
+
+class Drel2(TemplateView):
+    template_name = 'home/d-rel-2.html'
     
+class Dfem1(TemplateView):
+    template_name = 'home/d-fem-1.html'
+
 class Dtrel1(TemplateView):
     template_name = 'home/dt-rel-1.html'
